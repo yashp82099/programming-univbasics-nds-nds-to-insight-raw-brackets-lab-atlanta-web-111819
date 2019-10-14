7958a3c10cb271e0fc9fae357386138cc49c640d
yash patel <yash82099@yahoo.com>
2019-10-14 00:31:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,6 +11,7 @@ def directors_totals(nds)
   
   result = {
   }
+  hash = {}
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
@@ -31,6 +32,8 @@ def directors_totals(nds)
         pp nds[x][:movies][z][:worldwide_gross]
         z += 1 
       end 
+      
+      
       y +=1
     end
      x += 1 
